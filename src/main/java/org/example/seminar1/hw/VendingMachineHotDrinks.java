0310b6ca7f3d1cb158083984d17fc8f116392993
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-03-10 21:45:28 +0500

    seminar1

--- src/main/java/org/example/seminar1/hw/VendingMachineHotDrinks.java
+++ src/main/java/org/example/seminar1/hw/VendingMachineHotDrinks.java
@@ -11,7 +11,7 @@ public class VendingMachineHotDrinks extends VendingMachine{
     }
 
     @Override
-    String getProduct(String name) {        // метод для вывода горячих напитков с названием name (переопределяем родительский)
+    String getProduct(String name) {
         for (HotDrinks hd : hotDrinksList) {
             if (hd.getName().contains(name)) return hd.getName() + ", " + hd.getPrice();
         }
